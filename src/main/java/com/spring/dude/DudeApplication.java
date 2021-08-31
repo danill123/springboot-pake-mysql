d81f88da6fe9c90762e8add16a5f9ed0654ff3b8
Danill Yudhistira Setiawan <danillyudhistira@gmail.com>
2021-08-31 16:33:09 +0700

    update

--- src/main/java/com/spring/dude/DudeApplication.java
+++ src/main/java/com/spring/dude/DudeApplication.java
@@ -2,7 +2,6 @@ package com.spring.dude;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/* start apps */
 @SpringBootApplication
 public class DudeApplication {
 
